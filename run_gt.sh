d29143822d5d975d3b90d25db70a6ed5caa3cb68
root <root@localhost.localdomain>
2025-09-05 15:51:57 +0800

    update

--- run_gt.sh
+++ run_gt.sh
@@ -1,6 +1,7 @@
 #/bin/bash
 file_name=/home/vllm-ascend-origin/vllm_ascend/core/schedule_config.py
 log_file="log_gt.txt"
+rm -f $log_file
 sed -i 's/scheduler_config\["enable_chunked_prefill"\] = True/scheduler_config["enable_chunked_prefill"] = False/' $file_name
 export VLLM_ASCEND_KV_DEBUG=1  && export VLLM_ASCEND_CHUNKED_PREFILL=0 && python test_ds_2.py >> $log_file
 
