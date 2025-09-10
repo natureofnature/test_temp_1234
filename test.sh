9cfe17971418c9c001702ea4a5321d567b75fed0
wzliu <wzliu>
2025-09-10 16:37:57 +0800

    update

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #rm -rf /tmp/kv_o*
 #rm -rf /tmp/vllm_ascend_kv_dump_*
-bash run_gt.sh  
+#bash run_gt.sh  
 bash run_chunked_prefill.sh  
 grep log_gt.txt -rne "Generated" && echo "===" && grep log_chunked.txt -rne "Generated" && echo "===" && grep -oP '(?<=for request).*' log_gt.txt | uniq  | grep -oP '(?<=: ).*' |sort | uniq 
 #grep log_gt.txt -rne "Generated"
